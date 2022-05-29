46bb0429852c10e4ab9b5c3ef20c953449d17081
ameliabridges <amelia.bridges@plymouth.ac.uk>
2022-05-29 14:29:15 +0100

    Initial site build completion

--- Useful_code.R
+++ Useful_code.R
@@ -9,6 +9,12 @@ create_post("UKOT_divgrads", collection = 'publications')
 create_post("TdC_bluecarbon", collection = 'publications')
 create_post("Bluenose", collection = 'publications')
 
+# create a new collection
+create_post("Post name", collection = 'collection name')
+# then you also need to put a .Rmd in the repo folder with the title and listing (from the YAML)
+# only, e.g.  title: Publications
+#             listing: publications
+
 # to make a new (non-nested) page that links in the navbar, all you need is to create a RMD file
 # in the repo folder called, for example, projects.Rmd. This file should have a YAML along the lines
 # of:
